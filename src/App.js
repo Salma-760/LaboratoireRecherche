7936f299984ce41b33ee8678911d548393f2f734
Salma <HP@DESKTOP-CTQESM4>
2025-03-04 15:11:02 +0100

    Description des changements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 
+
 import './App.css';
 import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import correct
-import Home from './pages/Home';
-import About from './pages/About';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct import for React Router v6
+import Barre from './Barre'; // Ton composant Barre
+import Accueil from './pages/Accueil';
+import Evenements from './pages/Evenements';
+import Actualites from './pages/Actualites';
+import Presentation from './pages/Presentation';
+import Recherche from './pages/Recherche';
+import Publications from './pages/Publications';
+import Formation from './pages/Formation';
+import Emplois from './pages/Emplois';
 
 function App() {
   return (
-
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div>
+        <Navbar />
+       
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/evenements" element={<Evenements />} />
+          <Route path="/actualites/actualites" element={<Actualites />} />
+          <Route path="/presentation/equipe" element={<Presentation />} />
+          <Route path="/recherche/projets" element={<Recherche />} />
+          <Route path="/publications/articles" element={<Publications />} />
+          <Route path="/formation/cours" element={<Formation />} />
+          <Route path="/emplois/offres" element={<Emplois />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
