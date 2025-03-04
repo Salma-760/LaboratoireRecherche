9e75013a964f3c185add1699758d4e87d89f8430
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2025-03-04 13:25:38 +0000

    Ajout logo et footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-
-import './App.css';
-import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import correct
-import Home from './pages/Home';
-import About from './pages/About';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Logo from "./components/Logo";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        {/* Affichage du Logo */}
+        <Logo />
+
+        {/* Navbar collée au Logo */}
+        <div className="relative z-10">
+          <Navbar />
+        </div>
+
+        {/* Contenu principal avec flex-grow pour remplir l'espace */}
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+
+        {/* Footer toujours en bas */}
+        <Footer />
+      </div>
     </Router>
   );
 }
